eef0abcc340b0791c7e45f177b8a1af2ca0b4dd2
gonzalogaticao <gonzalogaticaosorio@gmail.com>
2023-12-31 09:35:17 -0300

    show crud

--- src/Form/ShowType.php
+++ src/Form/ShowType.php
@@ -2,33 +2,28 @@
 
 namespace App\Form;
 
-use App\Entity\Publicity;
+use App\Entity\Show;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class PublicityType extends AbstractType
+class ShowType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('sentence')
-            ->add('background')
-            ->add('duration')
-            ->add('description')
-            //->add('Stock')
-            ->add('customer')
-            ->add('stock')
-            //->add('Guideline') //->Show
-            ->add('Show')
-            ->add('Audience')
+            ->add('name')
+            ->add('start')
+            ->add('finish')
+            ->add('date')
+            ->add('guideline')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Publicity::class,
+            'data_class' => Show::class,
         ]);
     }
 }
