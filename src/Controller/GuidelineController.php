1622976abba382a5943743a9af4accf841d8a8c2
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2023-12-31 14:04:24 -0300

    FINALLY MADE A MODAL FORM

--- src/Controller/GuidelineController.php
+++ src/Controller/GuidelineController.php
@@ -7,6 +7,8 @@ use App\Form\GuidelineType;
 use App\Repository\GuidelineRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,12 +16,56 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/guideline')]
 class GuidelineController extends AbstractController
 {
-    #[Route('/', name: 'app_guideline_index', methods: ['GET'])]
-    public function index(GuidelineRepository $guidelineRepository): Response
+    #[Route('/', name: 'app_guideline_index', methods: ['GET', 'POST'])]
+    public function index(GuidelineRepository $guidelineRepository, Request $req, EntityManagerInterface $entityManager, FormFactoryInterface $factory): Response
     {
+        /*
+        $guideline = new Guideline();
+        $form = $this->createForm(GuidelineType::class, $guideline);
+        $form->handleRequest($req);*/
+
+
+        /*
         //I dont like this way for doing this things but yeah, there is nothing to do about these deadlines.
+        if($req->getMethod() === "POST"){
+            if ($form->isSubmitted() && $form->isValid()) {
+                $entityManager->flush();
+    
+                return $this->redirectToRoute('app_guideline_index', [], Response::HTTP_SEE_OTHER);
+            }
+        }*/
+
+        $allGuidelines = $guidelineRepository->findAll();
+        $allForms = [];
+
+        foreach ($allGuidelines as $guideline) {
+            $form = $factory->createNamed(sprintf("guideline_%s", $guideline->getId()), GuidelineType::class, $guideline);
+        
+            $form->add('saveEdit', SubmitType::class, [
+                'label' => "Guardar cambios",
+                'attr' => [
+                    'class' => 'btn btn-primary'
+                ]
+            ]);
+
+            $form->handleRequest($req);
+            if ($req->getMethod() === "POST") {
+
+                if ($form->isSubmitted() && $form->isValid()) {
+                    $entityManager->persist($guideline);
+                    $entityManager->flush();
+                }
+            }
+
+            $allForms[] = [
+                //'guideline_' => $guideline,
+                'form' => $form->createView(),
+            ];
+        }
+
         return $this->render('guideline/crud_guideline.html.twig', [
-            'guidelines' => $guidelineRepository->findAll(),
+            'guidelines' => $allGuidelines,
+            'allForms' => $allForms,
         ]);
     }
 
--- src/Form/GuidelineType.php
+++ src/Form/GuidelineType.php
@@ -3,7 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Guideline;
+use App\Entity\Manager;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,12 +17,29 @@ class GuidelineType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        //dd($options);
         $builder
-            ->add('show_name')
-            ->add('emission_number')
-            ->add('creation_date')
-            ->add('broadcaster')
-            ->add('manager')
+            ->add('show_name', TextType::class, [
+                'attr' => ['class' => 'form-control'],
+                'label' => 'Nombre del programa'
+                ])
+            ->add('emission_number', NumberType::class, [
+                'attr' => ['class' => 'form-control'],
+                'label' => 'Número de emisión'
+                ])
+            ->add('creation_date', DateType::class, [
+                'attr' => ['class' => 'form-control'],
+                'label' => 'Fecha de emisión'
+                ])
+            ->add('broadcaster', ChoiceType::class, [
+                'attr' => ['class' => 'form-control'],
+                'label' => 'Locutor del programa'
+                ])
+            ->add('manager', EntityType::class, [
+                'class' => Manager::class,
+                'attr' => ['class' => 'form-control'],
+                'label' => 'Manager del programa',
+                ])
         ;
     }
 
