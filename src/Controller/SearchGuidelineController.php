99e6153b19188fdeb80e148e7f4b58ac2566ced6
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2023-12-31 21:08:37 -0300

    removed unneeded stuff

--- src/Controller/SearchGuidelineController.php
+++ src/Controller/SearchGuidelineController.php
@@ -11,7 +11,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class SearchGuidelineController extends AbstractController
 {
