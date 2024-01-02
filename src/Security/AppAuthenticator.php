0edf56b93aa1397fc1aa00423c2e61b464667a68
gonzalogaticao <gonzalogaticaosorio@gmail.com>
2024-01-02 19:58:21 -0300

    redirection + some buttons

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -49,7 +49,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('app_webpage'));
+        return new RedirectResponse($this->urlGenerator->generate('app_guideline_index'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
