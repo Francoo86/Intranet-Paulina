ed5bb7136dff467244cbf8f8c3cb683c1750f94f
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-01-02 05:01:24 -0300

    remove managercontroller

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
+use App\Helper;
+use App\Repository\ManagerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
+#[Route('/api')]
 class APIController extends AbstractController
 {
-    #[Route('/a/p/i', name: 'app_a_p_i')]
+    #[Route('/', name: 'api')]
     public function index(): Response
     {
-        return $this->render('api/index.html.twig', [
-            'controller_name' => 'APIController',
-        ]);
+        return $this->json(['message' => 'Here be dragons!']);
+    }
+    
+    #[Route('/manager', name: 'api_manager', methods: ['GET'])]
+    public function APIManagers(ManagerRepository $managerRepository, SerializerInterface $serial): Response
+    {
+        $allManagers = $managerRepository->findAll();
+    
+        $jsonObject = $serial->serialize($allManagers, 'json', [    
+            'circular_reference_handler' => function ($object) {
+            return $object->getId();
+        }]);
+
+        return new Response($jsonObject, 200, ['Content-Type' => 'application/json']);
     }
 }
