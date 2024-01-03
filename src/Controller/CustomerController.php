755631e3dab75f2f98d47784ef2bd0cb56019a30
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-01-02 21:30:09 -0300

    add twig for customers

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -6,6 +6,7 @@ use App\Entity\Customer;
 use App\Form\CustomerType;
 use App\Helper;
 use App\Repository\CustomerRepository;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormFactoryInterface;
@@ -65,58 +66,13 @@ class CustomerController extends AbstractController
             'creationForm' => $creationForm
         ]);
     }
-    
-    #[Route('/new', name: 'app_customer_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $customer = new Customer();
-        $form = $this->createForm(CustomerType::class, $customer);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($customer);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_customer_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('customer/new.html.twig', [
-            'customer' => $customer,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_customer_show', methods: ['GET'])]
-    public function show(Customer $customer): Response
-    {
-        return $this->render('customer/show.html.twig', [
-            'customer' => $customer,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_customer_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Customer $customer, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(CustomerType::class, $customer);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_customer_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('customer/edit.html.twig', [
-            'customer' => $customer,
-            'form' => $form,
-        ]);
-    }
 
     #[Route('/{id}', name: 'app_customer_delete', methods: ['POST'])]
     public function delete(Request $request, Customer $customer, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$customer->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($customer);
+            //$entityManager->remove($customer);
+            $customer->setDeletedAt(new DateTimeImmutable());
             $entityManager->flush();
         }
 
