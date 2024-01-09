b17523e4eae0488f1544d762d4b98b367876498f
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-01-08 23:02:16 -0300

    add new template to remove the ugly old one
    adjusting requirements

---
+++ dashboard_template/assets/js/pages/pricing.init.js
@@ -0,0 +1,1 @@
+function check(){for(var e=document.getElementById("plan-switch"),l=document.getElementsByClassName("month"),n=document.getElementsByClassName("annual"),t=0;t<l.length;t++)1==e.checked?(n[t].style.display="block",l[t].style.display="none"):0==e.checked&&(n[t].style.display="none",l[t].style.display="block")}check();
---
+++ dashboard_template/assets/js/pages/user-settings.init.js
@@ -0,0 +1,1 @@
+flatpickr("#datepicker-range",{mode:"range",altInput:!0,altFormat:"Y"}),new Choices("#choices-multiple-skill-input",{removeItemButton:!0});
