073cc02257e08f7439ab24668a78716d97c290ea
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-01-09 01:19:35 -0300

    test controllers

---
+++ src/Controller/PersonController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PersonController extends AbstractController
+{
+    #[Route('/person', name: 'app_person')]
+    public function index(): Response
+    {
+        return $this->render('person/index.html.twig', [
+            'controller_name' => 'PersonController',
+        ]);
+    }
+}
