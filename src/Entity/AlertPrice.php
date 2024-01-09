e6b81e604a07bcb2d3a904ba8fdb80cb774fcf92
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-01-08 22:19:19 -0300

    price entity (should be singleton)

---
+++ src/Entity/AlertPrice.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AlertPriceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AlertPriceRepository::class)]
+class AlertPrice
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $price = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
---
+++ src/Repository/AlertPriceRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\AlertPrice;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<AlertPrice>
+ *
+ * @method AlertPrice|null find($id, $lockMode = null, $lockVersion = null)
+ * @method AlertPrice|null findOneBy(array $criteria, array $orderBy = null)
+ * @method AlertPrice[]    findAll()
+ * @method AlertPrice[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class AlertPriceRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, AlertPrice::class);
+    }
+
+//    /**
+//     * @return AlertPrice[] Returns an array of AlertPrice objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('a')
+//            ->andWhere('a.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('a.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?AlertPrice
+//    {
+//        return $this->createQueryBuilder('a')
+//            ->andWhere('a.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
